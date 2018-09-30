53a18c9169cca0114cfef2e898ca9a9376b439a9
bashCandicoot <ste7@protonmail.com>
2018-09-30 22:34:54 +0100

    add the temperature and weather text

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,5 +1,9 @@
 <template>
-      <canvas ref="canvas"></canvas>
+  <div>
+    <div class="City-temp">{{this.cityTemp}}</div>
+    <div class="City-weather">{{this.cityWeather}}</div>
+    <canvas ref="canvas"></canvas>
+  </div>
 </template>
 
 <script>
@@ -11,8 +15,7 @@ export default {
   name: 'Weather',
   data: function() {
     return {
-      weather: null,
-      matrix: null,
+      city: null,
       cities: [
         'London',
         'Tokyo',
@@ -25,22 +28,22 @@ export default {
       ]
     };
   },
-  created() {
-    window.addEventListener('resize', this.updateCanvas, false)
-  },
   mounted() {
     setInterval( async () => {
       this.matrix.weather = await this.getWeather() 
       this.matrix.symbols = this.cityChars
-    }, 3000);
+    }, 8000);
     this.initCanvas() 
+  },
+  created() {
+    window.addEventListener('resize', this.updateCanvas, false)
   },
   methods: {
     async getWeather() {
-      const cities = this.cities.filter(city => city !== _.get(this, 'weather.city'))
+      const cities = this.cities.filter(city => city !== _.get(this, 'city.name'))
       const city = this.getRandomCity(cities);
       const weather = await WeatherService.getWeather(city);
-      this.weather = {...weather, city}
+      this.city = {...weather, name: city}
     },
     getRandomCity(cities) {
       return cities[Math.floor(Math.random() * Math.floor(cities.length))]
@@ -50,35 +53,45 @@ export default {
       this.matrix.setFont()
       this.matrix.setSymbolStartYPositions()
     },
-    initCanvas() {
+    async initCanvas() {
       const canvas = this.$refs.canvas
       const ctx = canvas.getContext('2d')
       this.matrix = new MatrixService({canvas, ctx})
       this.updateCanvas()
       this.matrix.drawSymbolsInterval()
+      this.matrix.weather = await this.getWeather() 
+      this.matrix.symbols = this.cityChars
     }
   },
   computed: {
     cityChars() {
-      return this.weather.city.split('').map(char => char.toUpperCase())
+      return `${this.city.name}-`.split('').map(char => char.toUpperCase())
+    },
+    cityTemp() {
+      return `${_.get(this, 'city.temp', '')}°C`
+    },
+    cityWeather() {
+      return _.get(this, 'city.text')
     }
   }
 }
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .City-temp {
+    font-size: 10vh;
+    position: absolute;
+    -webkit-transform: translate(-50%, 0); 
+    left: 50%;
+    top: 40%;
+    opacity: 0.3;
+  }
+  .City-weather {
+    font-size: 4vh;
+    position: absolute;
+    -webkit-transform: translate(-50%, 0); 
+    left: 50%;
+    top: 50%;
+    opacity: 0.3;
+  }
 </style>
