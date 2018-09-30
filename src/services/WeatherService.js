a1a96598a323bb02ee5e6eba30c56fa899c3b279
bashCandicoot <ste7@protonmail.com>
2018-09-30 21:22:23 +0100

    return city condition

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default {
-	getWeather({city}) {
+	async getWeather(city) {
 		const query = `select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='${city}') and u='c'`
-		axios.get(`https://query.yahooapis.com/v1/public/yql?q=${query}`)
+		const res = await axios.get(`https://query.yahooapis.com/v1/public/yql?q=${query}&format=json`)
+		return res.data.query.results.channel.item.condition
 	}
 }
