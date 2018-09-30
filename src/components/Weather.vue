4302ecc4cf13db1357b3502ebed885f5e33ac256
bashCandicoot <ste7@protonmail.com>
2018-09-30 21:21:37 +0100

    get random cities

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,28 +1,66 @@
 <template>
-  <div>
-    {{ weather }}
-  </div>
+      <canvas ref="canvas"></canvas>
 </template>
 
 <script>
 import WeatherService from '@/services/WeatherService';
+import MatrixService from '@/services/MatrixService'
+import _ from 'lodash'
 
 export default {
   name: 'Weather',
   data: function() {
     return {
-      weather: null
+      weather: null,
+      matrix: null,
+      cities: [
+        'London',
+        'Tokyo',
+        'Melbourne',
+        'Montreal',
+        'Paris',
+        'Munich',
+        'Berlin',
+        'Zurich',
+      ]
     };
   },
-  // props: {
-  //   msg: String
-  // },
   created() {
-    this.getWeather({city:'London'})
+    window.addEventListener('resize', this.updateCanvas, false)
+  },
+  mounted() {
+    setInterval( async () => {
+      this.matrix.weather = await this.getWeather() 
+      this.matrix.symbols = this.cityChars
+    }, 3000);
+    this.initCanvas() 
   },
   methods: {
-    getWeather(city) {
-      this.weather = WeatherService.getWeather(city);
+    async getWeather() {
+      const cities = this.cities.filter(city => city !== _.get(this, 'weather.city'))
+      const city = this.getRandomCity(cities);
+      const weather = await WeatherService.getWeather(city);
+      this.weather = {...weather, city}
+    },
+    getRandomCity(cities) {
+      return cities[Math.floor(Math.random() * Math.floor(cities.length))]
+    },
+    updateCanvas() {
+      this.matrix.getCanvasSize()
+      this.matrix.setFont()
+      this.matrix.setSymbolStartYPositions()
+    },
+    initCanvas() {
+      const canvas = this.$refs.canvas
+      const ctx = canvas.getContext('2d')
+      this.matrix = new MatrixService({canvas, ctx})
+      this.updateCanvas()
+      this.matrix.drawSymbolsInterval()
+    }
+  },
+  computed: {
+    cityChars() {
+      return this.weather.city.split('').map(char => char.toUpperCase())
     }
   }
 }
